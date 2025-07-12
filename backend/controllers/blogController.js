42d3f15bce8aa2059d84b91e1bba325a9521b82b
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-07-13 00:04:18 +0600

    blog upload api successfully working

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,51 +1,58 @@
-import fs from "fs"
+import fs from "fs";
 import imagekit from "../configs/imageKit.js";
 import Blog from "../models/Blog.js";
 
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.blog
+    );
+    const imageFile = req.file;
 
-
-export const addBlog = async(req, res)=>{
-    try {
-        const {title,subTitle, description, category,isPublished} = JSON.parse(req.body.blog)
-        const imgFile = req.file;
-
-
-        // check if all the fields are present 
-        if(!title || !description || !category || !isPublished || !subTitle || !imgFile){
-            return res.json({success: false, message: "Missing Required Fields"})
-        }
-
-        const fileBuffer = fs.readFileSync(imageFile.path)
-
-        // upload image to image kit 
-        const response = await imagekit.upload({
-            file : fileBuffer,
-            fileName: imageFile.originalName,
-            folder: "/blogs"
-        })
-
-        // optimize through imagekit URL transformation 
-        const optimizeImageUrl = imagekit.url({
-            path: response.filePath,
-            transformation: [
-                {quality: 'auto'}, //auto compression
-                {format: "webp"}, //convert to modern format 
-                {width: "1280"} //width resizing 
-            ]
-        })
-
-
-        const image = optimizeImageUrl;
-
-        await Blog.create({title, subTitle, description, category, image, isPublished})
-
-        res.json({success: true, message: "Blog added Successfully"})
-
-
-
-
-
-    } catch (error) {
-        res.json({success:false, message: error.message})
+    // check if all the fields are present
+    if (
+      !title ||
+      !description ||
+      !category ||
+      !isPublished ||
+      !subTitle ||
+      !imageFile
+    ) {
+      return res.json({ success: false, message: "Missing Required Fields" });
     }
-}
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+
+    // upload image to image kit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    // optimize through imagekit URL transformation
+    const optimizeImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: "auto" }, //auto compression
+        { format: "webp" }, //convert to modern format
+        { width: "1280" }, //width resizing
+      ],
+    });
+
+    const image = optimizeImageUrl;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ success: true, message: "Blog added Successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/BlogRoutes.js";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
     res.send("API is work");
 });
 app.use("/api/admin", adminRouter)
+app.use("/api/blog", blogRouter)
 
 const PORT = process.env.PORT || 3000;
 
