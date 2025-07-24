00193d7c936a6eaa711c6cd0ef016d4b55c3258e
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-07-24 20:47:12 +0600

    problem solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Blog from "./pages/Blog"
-import Layout from "./pages/admin/Layout"
-import Dashboard from "./pages/admin/Dashboard"
-import AddBlog from "./pages/admin/AddBlog"
-import Comments from "./pages/admin/Comments"
-import ListBlog from "./pages/admin/ListBlog"
-import Login from "./components/admin/Login"
-import "quill/dist/quill.snow.css"
-import { useAppContext } from "./context/appContext"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import Comments from "./pages/admin/Comments";
+import ListBlog from "./pages/admin/ListBlog";
+import Login from "./components/admin/Login";
+import "quill/dist/quill.snow.css";
+import { useAppContext } from "./context/AppContext";
 
 const App = () => {
-  const {token} = useAppContext()
+  const { token } = useAppContext();
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/blog/:id" element={<Blog />}></Route>
-        <Route path="/admin" element={token ?<Layout />: <Login />}>
+        <Route path="/admin" element={token ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
@@ -25,7 +25,7 @@ const App = () => {
         </Route>
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
